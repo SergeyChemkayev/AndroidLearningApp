cc90b17ba53e37a00b3f3bb29ffc47f178df5a7b
SergeyChemkayev <chemkayev.sergey@gmail.com>
2018-07-30 10:08:22 +0300

    added: item.xml -- settings for single item in recycler view
    Changed:
    RecyclerViewActivity -- added fillItemsList method and connect MyAdapter
    MyAdapter -- some changes in constructor and refactor the onCreateViewHolder method

--- app/src/main/java/com/example/androidlearningapp/MyAdapter.java
+++ app/src/main/java/com/example/androidlearningapp/MyAdapter.java
@@ -1,8 +1,10 @@
 package com.example.androidlearningapp;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -14,20 +16,24 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView textView;
 
-        public ViewHolder(TextView v) {
+        public ViewHolder(View v) {
             super(v);
-            textView = v;
+            textView = (TextView) itemView.findViewById(R.id.item_name);
         }
     }
+
     public MyAdapter(List<String> myDataSet) {
-        dataSet=myDataSet;
+        dataSet = myDataSet;
     }
 
     @Override
-    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-        TextView v = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_recycler_view, parent,false); //??????
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
 
-        return new ViewHolder(v);
+        View itemView = inflater.inflate(R.layout.item, parent, false);
+
+        return new ViewHolder(itemView);
     }
 
     @Override
--- app/src/main/java/com/example/androidlearningapp/RecyclerViewActivity.java
+++ app/src/main/java/com/example/androidlearningapp/RecyclerViewActivity.java
@@ -5,21 +5,32 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecyclerViewActivity extends AppCompatActivity {
-    /*private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;*/
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
-        /*recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         recyclerView.setHasFixedSize(true);
+        List<String> list = new ArrayList<>();
+        list = fillItemsList(list);
+        MyAdapter adapter = new MyAdapter(list);
+        recyclerView.setAdapter(adapter);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
-        layoutManager=new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);*/
+    }
 
+    private List<String> fillItemsList(List<String> list) {
+        for (int i = 0; i < 40; i++) {
+            list.add("Item #" + (i + 1));
+        }
+        return list;
     }
 }
