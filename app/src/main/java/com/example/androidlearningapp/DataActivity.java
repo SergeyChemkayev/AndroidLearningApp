5e7069bba03292a99e93ded5b3f0e3d502b67ef9
SergeyChemkayev <chemkayev.sergey@gmail.com>
2018-08-06 11:54:50 +0300

    activity_data.xml - progressBar removed, swipeRefreshLayout added; DataActivity - progressBar logic removed, swipeRefreshLayout logic added;

--- app/src/main/java/com/example/androidlearningapp/DataActivity.java
+++ app/src/main/java/com/example/androidlearningapp/DataActivity.java
@@ -2,11 +2,11 @@ package com.example.androidlearningapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.androidlearningapp.listeners.GetMoviesListener;
@@ -20,7 +20,7 @@ public class DataActivity extends AppCompatActivity implements GetMoviesListener
     private View emptyView;
     private MoviesAdapter adapter;
     private MoviesRemoteSource moviesRemoteSource = MoviesNetwork.getInstance();
-    private ProgressBar progressBar;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     public static void open(Context context) {
         Intent intent = new Intent(context, DataActivity.class);
@@ -33,7 +33,17 @@ public class DataActivity extends AppCompatActivity implements GetMoviesListener
         setContentView(R.layout.activity_data);
         recyclerView = (RecyclerView) findViewById(R.id.data_movies_recycler_view);
         emptyView = (View) findViewById(R.id.data_empty_view);
-        progressBar = (ProgressBar) findViewById(R.id.data_loading_progress_bar);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.data_swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getMovies();
+            }
+        });
+        swipeRefreshLayout.setColorSchemeResources(R.color.holo_blue_bright,
+                R.color.holo_green_light,
+                R.color.holo_orange_light,
+                R.color.holo_red_light);
         adapter = new MoviesAdapter();
         recyclerView.setAdapter(adapter);
         getMovies();
@@ -53,7 +63,6 @@ public class DataActivity extends AppCompatActivity implements GetMoviesListener
 
     @Override
     public void onGetMoviesSuccess(List<Movie> movies) {
-        progressBar.setVisibility(View.GONE);
         if (movies == null || movies.isEmpty()) {
             setViewsVisibility(true);
         } else {
@@ -64,12 +73,12 @@ public class DataActivity extends AppCompatActivity implements GetMoviesListener
 
     @Override
     public void onGetMoviesError(Throwable error) {
-        progressBar.setVisibility(View.GONE);
         setViewsVisibility(true);
         Toast.makeText(DataActivity.this, R.string.error_message, Toast.LENGTH_SHORT).show();
     }
 
     private void setViewsVisibility(boolean isMoviesEmpty) {
+        swipeRefreshLayout.setRefreshing(false);
         if (isMoviesEmpty) {
             recyclerView.setVisibility(View.GONE);
             emptyView.setVisibility(View.VISIBLE);
@@ -80,7 +89,7 @@ public class DataActivity extends AppCompatActivity implements GetMoviesListener
     }
 
     private void getMovies() {
-        progressBar.setVisibility(View.VISIBLE);
+        swipeRefreshLayout.setRefreshing(true);
         moviesRemoteSource.getMovies();
     }
 }
