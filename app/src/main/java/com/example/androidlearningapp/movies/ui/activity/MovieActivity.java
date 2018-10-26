0c3c211c79ae2dbd1189033d1e030f1a3fa8d5d6
SergeyChemkayev <chemkayev.sergey@gmail.com>
2018-10-26 12:59:09 +0300

    coordinator layout and collapsing toolbar setup

--- app/src/main/java/com/example/androidlearningapp/movies/ui/activity/MovieActivity.java
+++ app/src/main/java/com/example/androidlearningapp/movies/ui/activity/MovieActivity.java
@@ -4,13 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.androidlearningapp.R;
 import com.example.androidlearningapp.movies.entity.Movie;
 
 public class MovieActivity extends AppCompatActivity {
+    private ImageView movieCoverView;
+    private TextView movieDescriptionView;
+    private Toolbar movieNameToolbar;
+
     private Movie movie;
+
 
     public static void open(Context context, Movie movie) {
         Intent intent = new Intent(context, MovieActivity.class);
@@ -22,7 +31,18 @@ public class MovieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+        movieCoverView = findViewById(R.id.movie_cover_toolbar_image_view);
+        movieDescriptionView = findViewById(R.id.movie_description_text_view);
+        movieNameToolbar = findViewById(R.id.movie_name_toolbar);
         movie = getIntent().getParcelableExtra("movie");
-        Toast.makeText(this, movie.getName(), Toast.LENGTH_SHORT).show();
+        movieNameToolbar.setTitle(movie.getName());
+        setSupportActionBar(movieNameToolbar);
+        Glide.with(this)
+                .load(movie.getImage())
+                .apply(new RequestOptions().centerCrop())
+                .into(movieCoverView);
+        movieDescriptionView.setText(movie.getDescription());
     }
+
+
 }
