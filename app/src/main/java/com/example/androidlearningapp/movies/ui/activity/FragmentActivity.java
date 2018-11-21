bbe65f60537b98a066b83f47f258634c7e1002da
SergeyChemkayev <chemkayev.sergey@gmail.com>
2018-11-21 09:10:13 +0300

    menu added to DataActivity, FragmentActivity created

---
+++ app/src/main/java/com/example/androidlearningapp/movies/ui/activity/FragmentActivity.java
@@ -0,0 +1,34 @@
+package com.example.androidlearningapp.movies.ui.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.example.androidlearningapp.R;
+
+public class FragmentActivity extends AppCompatActivity {
+
+    public static void open(Context context) {
+        Intent intent = new Intent(context, FragmentActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
