0584cab5e985bd39d1551baad600ac045ad42a58
SergeyChemkayev <chemkayev.sergey@gmail.com>
2018-11-26 13:34:38 +0300

    two broadcast receivers added

---
+++ app/src/main/java/com/example/androidlearningapp/movies/data/api/listeners/PowerStatusReceiver.kt
@@ -0,0 +1,16 @@
+package com.example.androidlearningapp.movies.data.api.listeners
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class PowerStatusReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        when (intent.action) {
+            "android.intent.action.ACTION_POWER_CONNECTED", "android.intent.action.ACTION_POWER_DISCONNECTED" -> Toast.makeText(context, "Power status changed", Toast.LENGTH_SHORT).show()
+            else -> return
+        }
+    }
+}
--- app/src/main/java/com/example/androidlearningapp/movies/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/androidlearningapp/movies/ui/activity/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.androidlearningapp.movies.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.androidlearningapp.R;
 
@@ -16,6 +21,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.main_open_data_activity_button);
         button.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (Intent.ACTION_AIRPLANE_MODE_CHANGED.equals(intent.getAction())) {
+                    Toast.makeText(context, "Airplane mode changed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        registerReceiver(receiver, new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED));
     }
 
     @Override
